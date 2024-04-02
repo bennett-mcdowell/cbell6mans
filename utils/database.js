3a7e64da847ad97c623760165e9d8061a76615a1
Bennett McDowell <52725971+bennett-mcdowell@users.noreply.github.com>
2024-04-02 12:50:31 -0400

    deployment changes

---
+++ utils/database.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  schema: process.env.DB_SCHEMA,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  },
+});
+
+module.exports = pool;
