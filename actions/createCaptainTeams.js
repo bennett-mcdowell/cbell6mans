033e7f22afd4d0de3ef66400fe2e305d0a92b004
Bennett McDowell <52725971+bennett-mcdowell@users.noreply.github.com>
2024-04-02 21:55:18 -0400

    fix: bug where button was not switching pages and added captains test

--- actions/createCaptainTeams.js
+++ actions/createCaptainTeams.js
@@ -1,5 +1,124 @@
+const { ActionRowBuilder, EmbedBuilder, ButtonBuilder} = require('discord.js');
+const { getQueueList, getPlayerStats } = require('../utils/retrieveFromDatabase');
+const { updateTeamInlobby_players } = require('../utils/updateInDatabase');
+const client = require('../index');
 
+const createVoiceChannel = require('./createVoiceChannel');
 
 module.exports = async (interaction) => {
+	// Fetch players in the queue
+	let players = await getQueueList();
 
+	// Select two random captains
+	const captains = players.sort(() => 0.5 - Math.random()).slice(0, 2);
+
+	// Remove captains from available players
+	let availablePlayers = players.filter(player => !captains.includes(player));
+
+	async function handleCaptainSelection(captain) {
+		const captainName = await client.users.fetch(captain);
+		const embed = new EmbedBuilder()
+			.setTitle('Choose your team')
+			.setDescription('Click on the buttons to choose players for your team.');
+
+		// Add players to the embed
+		availablePlayers.forEach(player => {
+			const stats = getPlayerStats(player);
+			embed.addFields(player.name, stats, true);
+		});
+
+		const row = new ActionRowBuilder()
+			.addComponents(availablePlayers.map(player =>
+				new ButtonBuilder()
+					.setCustomId(player.id.toString())
+					.setLabel(player.name)
+					.setStyle('PRIMARY')
+			));
+
+		const message = await captainName.send({ embeds: [embed], components: [row] });
+		const filter = (i) => i.user.id === captain.id && availablePlayers.some(player => player.id.toString() === i.customId);
+
+		const collector = message.createMessageComponentCollector({ filter, max: 1, time: 60000 });
+		return new Promise((resolve, reject) => {
+			collector.on('collect', async (i) => {
+				const selectedPlayerId = i.customId;
+				const selectedPlayer = availablePlayers.find(player => player.id.toString() === selectedPlayerId);
+				availablePlayers = availablePlayers.filter(player => player.id.toString() !== selectedPlayerId);
+				await i.update({ content: `You have selected ${selectedPlayer.name}.`, embeds: [], components: [] });
+				resolve(selectedPlayer);
+			});
+
+			collector.on('end', collected => {
+				if (collected.size === 0) {
+					captainName.send('You took too long to select a player.');
+					reject(new Error('Selection timeout'));
+				}
+			});
+		});
+	}
+
+	// Initialize teams
+	const team1 = [];
+	const team2 = [];
+
+	// First pick
+	const firstPick = captains[0];
+	const firstPickedPlayer = await handleCaptainSelection(firstPick);
+	team1.push(firstPickedPlayer); // Add picked player to team1
+	await updateTeamInlobby_players(firstPickedPlayer.id, 1);
+
+	// Second pick
+	const secondPick = captains[1];
+	const secondPickedPlayers = [];
+	for (let i = 0; i < 2; i++) {
+		const pickedPlayer = await handleCaptainSelection(secondPick);
+		secondPickedPlayers.push(pickedPlayer);
+		team2.push(pickedPlayer); // Add picked player to team2
+		await updateTeamInlobby_players(pickedPlayer[i].id, 2);
+	}
+
+	// Add remaining player to first pick's team
+	const remainingPlayer = availablePlayers[0];
+	if (remainingPlayer) {
+		team1.push(remainingPlayer);
+		await updateTeamInlobby_players(remainingPlayer.id, 1);
+	}
+
+
+	// Display teams
+	const embed = new EmbedBuilder()
+		.setTitle('Teams')
+		.setTimestamp()
+		.setColor(0x0099ff);
+
+	// Create a mention string for each user in team1
+	const mentionListTeam1 = team1.map((userId) => {
+		const user = interaction.guild.members.cache.get(userId);
+		return user ? user.toString() : '';
+	});
+
+	// Create a mention string for each user in team2
+	const mentionListTeam2 = team2.map((userId) => {
+		const user = interaction.guild.members.cache.get(userId);
+		return user ? user.toString() : '';
+	});
+
+	// Join the mention list with commas to display in the embed
+	const mentionStringTeam1 = mentionListTeam1.join(', ');
+	const mentionStringTeam2 = mentionListTeam2.join(', ');
+
+	if (mentionListTeam1) {
+		embed.addFields({ name: 'Team 1: ', value: mentionStringTeam1 });
+	}
+	if (mentionListTeam2) {
+		embed.addFields({ name: 'Team 2: ', value: mentionStringTeam2 });
+	}
+
+	// Get the channel to send the message
+	const channel = interaction.channel;
+
+	// Send the message
+	await channel.send({ embeds: [embed.toJSON()] });
+
+	createVoiceChannel(interaction);
 };
--- commands/game/leaderboard.js
+++ commands/game/leaderboard.js
@@ -10,7 +10,7 @@ module.exports = {
 			option.setName('rank')
 				.setDescription('Enter the rank you want to see, leave blank to see all ranks')),
 	async execute(interaction) {
-		await interaction.deferReply(); // Use deferReply for asynchronous handling
+		await interaction.deferReply();
 		const rank = interaction.options.getString('rank') || 'all';
 		const page = 0; // Start from the first page
 
@@ -27,7 +27,13 @@ async function getTotalPlayerCount(rank) {
 }
 
 async function sendLeaderboardPage(interaction, rank, page) {
-	const itemsPerPage = 10;
+	// Check if the interaction is valid
+	if (!interaction.deferred && !interaction.replied) {
+		console.error('Interaction is invalid or already replied.');
+		return;
+	}
+
+	const itemsPerPage = 1;
 	const playerStatsPage = await getPlayerStatsPage(rank, page, itemsPerPage);
 	const totalPlayers = await getTotalPlayerCount(rank);
 	const totalPages = Math.ceil(totalPlayers / itemsPerPage);
@@ -67,22 +73,31 @@ async function sendLeaderboardPage(interaction, rank, page) {
 				.setDisabled(page >= totalPages - 1),
 		);
 
-	await interaction.editReply({ embeds: [embed], components: [row] });
+	try {
+		// Attempt to edit the reply
+		await interaction.editReply({ embeds: [embed], components: [row] });
 
-	const filter = i => ['previous_page', 'next_page'].includes(i.customId) && i.user.id === interaction.user.id;
-	const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+		const filter = i => ['previous_page', 'next_page'].includes(i.customId) && i.user.id === interaction.user.id;
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
 
-	collector.on('collect', async i => {
-		let newPage = page;
-		if (i.customId === 'previous_page' && page > 0) {
-			newPage--;
-		} else if (i.customId === 'next_page' && page < totalPages - 1) {
-			newPage++;
+		collector.on('collect', async i => {
+			let newPage = page;
+			if (i.customId === 'previous_page' && page > 0) {
+				newPage--;
+			} else if (i.customId === 'next_page' && page < totalPages - 1) {
+				newPage++;
+			}
+
+			await sendLeaderboardPage(interaction, rank, newPage);
+		});
+
+		collector.on('end', () => interaction.editReply({ components: [] }));
+	} catch (error) {
+		// Handle InteractionFailedError
+		if (error.code === 10008 || error.code === 10015) {
+			console.error('Interaction failed: The message was deleted or the interaction no longer exists.');
+		} else {
+			console.error('Interaction failed:', error);
 		}
-
-		await i.deferUpdate();
-		await sendLeaderboardPage(interaction, rank, newPage);
-	});
-
-	collector.on('end', () => interaction.editReply({ components: [] }));
+	}
 }
